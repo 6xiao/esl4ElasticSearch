0a71e555564b881ec391187d63933e176e748d9d
Albert Liu <liuxiao89@outlook.com>
2015-02-12 15:01:07 +0800

    use common code

--- EasySearch/EasySearch.go
+++ EasySearch/EasySearch.go
@@ -2,30 +2,24 @@ package EasySearch
 
 import (
 	"errors"
-	"log"
-	"net"
-	"net/rpc"
 )
 
 type EasySearch struct {
-	Appkey   string
-	Platform string
-	Index    string
-	Type     string
-	Fields   []string
-	ESL      string
-	SysCond  string
+	Appkey string
+	Index  string
+	Type   string
+	Fields []string
+	ESL    string
 }
 
 type SearchInvoke func(EasySearch, *map[string][]byte) error
 
 type ESRPC struct {
-	Address    string
 	SearchFunc SearchInvoke
 }
 
-func NewEsRpc(addr string, sf SearchInvoke) *ESRPC {
-	return &ESRPC{addr, sf}
+func NewEsRpc(sf SearchInvoke) *ESRPC {
+	return &ESRPC{sf}
 }
 
 func (es *ESRPC) Search(in EasySearch, out *map[string][]byte) error {
@@ -35,25 +29,4 @@ func (es *ESRPC) Search(in EasySearch, out *map[string][]byte) error {
 	}
 
 	return errors.New("can't search")
-}
-
-func ListenEsRpc(esrpc *ESRPC) {
-	defer log.Fatal("Big Error : Rpc Server quit")
-	defer CheckPanic()
-
-	server := rpc.NewServer()
-	server.Register(esrpc)
-
-	if listener, err := net.Listen("tcp", esrpc.Address); err != nil {
-		log.Println("Big Error : Rpc can't start", err)
-	} else {
-		log.Println("Task Rpc running @", esrpc.Address)
-		server.Accept(listener)
-	}
-}
-
-func CheckPanic() {
-	if err := recover(); err != nil {
-		log.Panicln("Panic : ", err)
-	}
 }
--- ElasticEasySearch/ElasticEasySearch.go
+++ ElasticEasySearch/ElasticEasySearch.go
@@ -5,28 +5,23 @@ import (
 	"flag"
 	"fmt"
 	"github.com/6xiao/esl4ElasticSearch/EasySearch"
+	"github.com/6xiao/go/Common"
 	es "github.com/mattbaird/elastigo/lib"
 	"log"
 	"runtime"
 )
 
 var (
-	flgRpcPort = flag.String("rpcport", ":12311", "rpc server port")
-	flgEs      = flag.String("es", "192.168.248.16", "elastic search")
+	flgRpc = flag.String("rpc", ":12311", "rpc server port")
+	flgEs  = flag.String("elasticsearch", "192.168.248.16", "elastic search")
 )
 
-func Search(esl EasySearch.EasySearch, res *map[string][]byte) error {
-	defer EasySearch.CheckPanic()
+func Search(ess EasySearch.EasySearch, res *map[string][]byte) error {
+	defer Common.CheckPanic()
 
-	ESL := esl.ESL
-	if len(esl.SysCond) > 0 {
-		ESL = "{" + esl.ESL + "} {" + esl.SysCond + "}"
-	}
-
-	filter, err := ParseEsl(ESL)
+	filter, err := ParseEsl(ess.ESL)
 	if filter == nil || err != nil {
-		log.Println("error parse esl :", err)
-		log.Println("error:", ESL)
+		log.Println("error parse esl :", err, ess.ESL)
 		return err
 	}
 
@@ -45,12 +40,8 @@ func Search(esl EasySearch.EasySearch, res *map[string][]byte) error {
 
 	c := es.NewConn()
 	c.Domain = *flgEs
-	se := es.Query().Term("Appkey", esl.Appkey)
-	re := es.Search(esl.Index).Type(esl.Type).Fields("")
-	if len(esl.Fields) > 0 {
-		re = re.Fields(esl.Fields...)
-	}
-
+	se := es.Query().Term("appkey", ess.Appkey)
+	re := es.Search(ess.Index).Type(ess.Type).Fields(ess.Fields...)
 	rsp, err := re.Size("65536").Scroll("1").Query(se).Filter(filter).Result(c)
 	if err != nil {
 		log.Println("error:", err)
@@ -89,7 +80,7 @@ func init() {
 }
 
 func main() {
-	trpc := EasySearch.NewEsRpc(*flgRpcPort, Search)
-	EasySearch.ListenEsRpc(trpc)
+	trpc := EasySearch.NewEsRpc(Search)
+	Common.ListenRpc(*flgRpc, trpc, nil)
 	log.Fatal("exit ...")
 }
